d9acf2e4adfe22f374db74156b2dcd08b15e633f
Test neto <nynekootonagi@gmail.com>
2024-09-28 19:58:10 +0300

    fail

---
+++ src/test/java/PostmanTest.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanTest {
+
+    @Test
+    public void shouldSendPostRequest() {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(220)
+                .body("data", equalTo("some data"));
+    }
+}
